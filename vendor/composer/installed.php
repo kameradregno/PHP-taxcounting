a84234c8aaf21d5d6a93df250b08fd20a17606db
kameradregno <regon0705@gmail.com>
2023-02-08 08:51:08 +0700

    materials

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,6 +1,6 @@
 <?php return array(
     'root' => array(
-        'name' => 'pph21/calculate',
+        'name' => 'pajak21/calculate',
         'pretty_version' => '1.0.0+no-version-set',
         'version' => '1.0.0.0',
         'reference' => NULL,
